ab5269d8aac8168487cc797d96d13d1e7574c1fc
Dixon <59415488+radixon@users.noreply.github.com>
2024-02-09 19:15:25 -0500

    Added hello_cuda.cu file
    The hello_cuda kernel.

---
+++ HelloWorld/hello_cuda.cu
@@ -0,0 +1,16 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+
+__global__ void hello_cuda(){
+    printf("Hello CUDA!\n");
+}
+
+int main(){
+    hello_cuda <<<1, 10>>>();
+    cudaDeviceSynchronize();
+
+    cudaDeviceReset();
+
+    return 0;
+}
